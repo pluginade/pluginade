96db22e8937f2ddb6cf7ed8fef38ecdffc2d48fd
Phil Johnston <phil.johnston@wpengine.com>
2024-01-16 15:26:46 -0500

    Remove debugging code

--- docker-phpunit/wp-config-docker.php
+++ docker-phpunit/wp-config-docker.php
@@ -32,15 +32,12 @@ if (!function_exists('getenv_docker')) {
 	// https://github.com/docker-library/wordpress/issues/588 (WP-CLI will load this file 2x)
 	function getenv_docker($env, $default) {
 		if ($fileEnv = getenv($env . '_FILE')) {
-		  echo 'using env file value';
 			return rtrim(file_get_contents($fileEnv), "\r\n");
 		}
 		else if (($val = getenv($env)) !== false) {
-		  echo 'using value from env variable';
 			return $val;
 		}
 		else {
-      echo 'using default';
 			return $default;
 		}
 	}
