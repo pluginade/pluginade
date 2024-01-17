f850361f9c189f63e59dffb531fb719106b851da
Phil Johnston <phil.johnston@wpengine.com>
2024-01-17 16:15:53 -0500

    Add new readme and example pluginade.sh file

--- pluginade.sh
+++ pluginade.sh
@@ -1,13 +1,6 @@
-# Pluginade Scripts
-Pluginade Scripts is a simple solution for adding linting, testing, zipping, and more to your WordPress plugin in seconds.
-
-It allows you to instantly run PHP WordPress Coding Standards, phpunit, eslint, stylelint and more, on any machine that supports Docker.
-
-# Getting Started
-
-## Create and commit this file to your WordPress plugin's root directory, and call it pluginade.sh
-```
 #!/bin/bash
+
+# This file lives in the root of a plugin directory to enable pluginade commands.
 
 # Change the following variables to your plugin's namespace and textdomain:
 textdomain="my-plugin-text-domain";
@@ -17,8 +10,21 @@ namespace="MyPluginNamespace";
 
 # Check if an argument is provided
 if [ -z "$1" ]; then
-    echo "Usage: $0 <The pluginade command you want to run>"
-    exit 1
+	# If no argument is provided, show the help text.
+	echo "Usage: sh pluginade.sh <The pluginade command you want to run>"
+	echo "Available commands:"
+	echo "lint:php      Run PHP Linting."
+	echo "lint:php:fix  Run PHP Lint Fixing."
+	echo "lint:css      Run CSS Linting."
+	echo "lint:css:fix  Run CSS Lint Fixing."
+	echo "lint:js       Run JS Linting."
+	echo "lint:js:fix   Run JS Lint Fixing."
+	echo "test:js       Run JS Jest Testing."
+	echo "test:phpunit  Run PHP Unit Testing."
+	echo "zip           Build an installable zip."
+	echo "dev           Run dev mode (npm run dev) for all wp-modules."
+	echo "build         Run build (npm run build) for all wp-modules."
+	exit 1
 fi
 
 #  Set the plugin directory to be the current directory.
@@ -86,50 +92,3 @@ fi
 if [ $1 == 'zip' ]; then
 	sh pluginade-run.sh -p "${plugindir}" -c "zip" -t $textdomain -n $namespace;
 fi
-```
-
-In the file above, simply replace these strings:
-
-- `my-plugin-text-domain` - The text domain to use for your plugin (Will be enforced by WordPress Coding Standards)
-- `MyPluginNamespace` - The unique namespace used for your plugin (Will be enforced as function/class prefix in WordPress Coding Standards)
-
-Then, on the command line inside your plugin, run one of the following commands:
-
-### To lint your plugin's PHP code using WordPress Coding Standards, run this command:
-`sh pluginade.sh lint:php`
-
-If you want to override any WordPress Coding Standards for your project, add your own custom phpcs.xml file to the root of your plugin, and Pluginade will use that.
-
-### To automatically fix any PHP issues that can be autofixed to conform with WordPress Coding Standards, run this command:
-`sh pluginade.sh lint:php:fix`
-
-### To lint your plugin's CSS code using WordPress's standards, run this command:
-`sh pluginade.sh lint:css`
-
-### To automatically fix any CSS issues that can be autofixed to conform with WordPress's standards, run this command:
-`sh pluginade.sh lint:css:fix`
-
-### To lint your plugin's Javascript code using WordPress's standards, run this command:
-`sh pluginade.sh lint:js`
-
-If you want to override any javascript standards in your own plugin, ass your own `.eslintrc` file to the root of your plugin, and Pluginade will use that.
-
-### To automatically fix any Javascript issues that can be autofixed to conform with WordPress's standards, run this command:
-`sh pluginade.sh lint:js:fix`
-
-### To run PHPUnit on your plugin's code, run this command:
-`sh pluginade.sh test:phpunit`
-
-### To run Jest tests on your plugin's Javascript code, run this command:
-`sh pluginade.sh test:js`
-
-### To build your plugin with webpack, run this command:
-`sh pluginade.sh build`
-
-### To put your plugin into webpack development mode, run this command:
-`sh pluginade.sh dev`
-
-### To create a zip of your plugin, run this command
-`sh pluginade.sh zip`
-
-Note that you can add a .zipignore file to the root of your plugin to control what files are not included in the zip. Pluginade includes a default zipignore which removes things usually not desired (like node_modules directories), but this can be overridden.
